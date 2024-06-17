0fa5b315a92e909983c68eb4b0e457e3a463d2a6
kidus-t-w <kidusk69@gmail.com>
2024-06-17 14:43:22 +0300

    Modified isAlive method

--- utils/redis.js
+++ utils/redis.js
@@ -1,37 +1,55 @@
-import { createClient } from 'redis';
 import { promisify } from 'util';
+import { createClient } from 'redis';
 
+/**
+ * Class for preforming connection with redis.
+ */
 class RedisClient {
   constructor() {
     this.client = createClient();
-
-    this.client.on('error', (err) => {
-      console.error(`Redis client not connected to the server: ${err.message}`);
-    });
-
-    this.client.on('connect', () => {
-      console.log('Redis client connected to the server');
-    });
-
     this.getAsync = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.set).bind(this.client);
-    this.delAsync = promisify(this.client.del).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis Client not connected to the server: ${error.message}`);
+    });
   }
 
+  /**
+   * Check if connection is alive
+   * @return { boolean } returns true if connections is alive else returns false
+   */
   isAlive() {
     return this.client.connected;
   }
 
+  /**
+   * Gets value of the desired key.
+   * @param {string} key key of the value to be returned
+   * @returns the value associated to the key.
+   */
   async get(key) {
-    return await this.getAsync(key);
+    const value = await this.getAsync(key);
+    return value;
   }
 
+  /**
+   * Creates a new key with a specific TTL
+   * @param {string} key key to be saved in redis
+   * @param {string} value value to be assigned to key
+   * @param {number} duration TTL of key
+   * @returns {undefined} No return
+   */
   async set(key, value, duration) {
-    await this.setAsync(key, value, 'EX', duration);
+    this.client.setex(key, duration, value);
   }
 
+  /**
+   *
+   * @param {string} key key to be deleted form redis.
+   * @return {undefined} No return
+   */
   async del(key) {
-    await this.delAsync(key);
+    this.client.del(key);
   }
 }
 
