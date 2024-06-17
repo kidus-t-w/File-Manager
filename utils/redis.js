104aa6fc978d49c85eebdc574e69af9fcc807450
kidus-t-w <kidusk69@gmail.com>
2024-06-17 11:11:21 +0300

    Modified logging

--- utils/redis.js
+++ utils/redis.js
@@ -1,32 +1,16 @@
-// utils/redis.js
-
 import { createClient } from 'redis';
 import { promisify } from 'util';
-import winston from 'winston';
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    // Adjusted the arrow function to return immediately
-    winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`),
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
 
 class RedisClient {
   constructor() {
     this.client = createClient();
 
     this.client.on('error', (err) => {
-      logger.error(`Redis client not connected to the server: ${err.message}`);
+      console.error(`Redis client not connected to the server: ${err.message}`);
     });
 
     this.client.on('connect', () => {
-      logger.info('Redis client connected to the server');
+      console.log('Redis client connected to the server');
     });
 
     this.getAsync = promisify(this.client.get).bind(this.client);
@@ -39,7 +23,7 @@ class RedisClient {
   }
 
   async get(key) {
-    return this.getAsync(key);
+    return await this.getAsync(key);
   }
 
   async set(key, value, duration) {
