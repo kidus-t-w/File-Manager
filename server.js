6a6da37ffbd1cb3903657bd97799fbd4afc5b4bd
kidus-t-w <kidusk69@gmail.com>
2024-06-18 10:04:46 +0300

    Created the first APi which handles /stats and /status endpoints

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routeController from './routes/index';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+routeController(app);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
