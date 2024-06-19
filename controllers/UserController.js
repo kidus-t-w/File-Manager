8d5f6248d06bde5fd29b4b5e01ded19d5e651c03
kidus-t-w <kidusk69@gmail.com>
2024-06-19 20:03:42 +0300

    Finished UserController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(request, response) {
@@ -33,6 +34,25 @@ class UsersController {
       }
     } catch (error) {
       console.log(error);
+      response.status(500).json({ error: 'Server error' });
+    }
+  }
+
+  static async getMe(request, response) {
+    try {
+      const userToken = request.headers('X-Token');
+      const authKey = `auth_${userToken}`;
+      const userID = await redisClient.get(authKey);
+      console.log('USER KEY GET ME', userID);
+
+      if (!userID) {
+        response.status(401).json({ error: 'Unauthorized' });
+      }
+      const user = await dbClient.getUser({ _id: ObjectId(userID) });
+      // console.log('USER GET ME', user);
+      response.json({ id: user._id, email: user.email });
+    } catch (err) {
+      console.log(err);
       response.status(500).json({ error: 'Server error' });
     }
   }
